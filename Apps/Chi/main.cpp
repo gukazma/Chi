3d0dba1515cb0db0bd34582d8d5bfb238a82a3f3
geekgary-x <18017158729@163.com>
2023-07-10 18:00:47 +0800

    🎉 init: init project

---
+++ Apps/Chi/main.cpp
@@ -0,0 +1,8 @@
+#include <Chi/Primitives/Timer.hpp>
+#include <Chi/SFM/InitImageListing.h>
+#include <iostream>
+#include <thread>
+int main(int argc, char* argv[])
+{
+    Chi::SFM::InitImageListing::main(argc, argv);
+}
---
+++ Modules/SFM/Include/Chi/SFM/InitImageListing.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace Chi {
+namespace SFM {
+namespace InitImageListing {
+int main(int argc, char** argv);
+}
+}   // namespace SFM
+}   // namespace Chi
