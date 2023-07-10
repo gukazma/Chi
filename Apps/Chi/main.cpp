d82a3ca214e2f5cbccab122da10eca76bab01ba6
gukazma <gukazma@proton.me>
2023-07-10 22:41:51 +0800

    ✨ feat: mainwindow

--- Apps/Chi/main.cpp
+++ Apps/Chi/main.cpp
@@ -1,8 +1,15 @@
+#include "Widgets/MainWindow.h"
 #include <Chi/Primitives/Timer.hpp>
 #include <Chi/SFM/InitImageListing.h>
+#include <QApplication>
 #include <iostream>
 #include <thread>
 int main(int argc, char* argv[])
 {
-    Chi::SFM::InitImageListing::main(argc, argv);
+    QApplication app(argc, argv);
+
+    MainWindow* mainwindow = new MainWindow;
+    mainwindow->showMaximized();
+
+    return app.exec();
 }
